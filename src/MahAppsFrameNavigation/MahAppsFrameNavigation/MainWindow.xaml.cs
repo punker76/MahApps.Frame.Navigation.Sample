04bbe1228d5c35c1a684070ad07819a3b3cc256b
punker76 <punkerat76@gmail.com>
2020-09-05 22:23:54 +0200

    Add sample

---
+++ src/MahAppsFrameNavigation/MahAppsFrameNavigation/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Navigation;
+using MahApps.Metro.Controls;
+using MahAppsFrameNavigation.Navigation;
+
+namespace MahAppsFrameNavigation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        private readonly Navigation.NavigationServiceEx _navigationServiceEx;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this._navigationServiceEx = NavigationServiceEx.Instance;
+            this._navigationServiceEx.Navigated += this.NavigationServiceEx_OnNavigated;
+            this.RootGrid.Children.Add(this._navigationServiceEx.Frame);
+
+            // Navigate to login page.
+            this.Loaded += (sender, args) =>
+                this._navigationServiceEx.Navigate(new Uri("Views/LoginPage.xaml", UriKind.RelativeOrAbsolute));
+        }
+
+        private void NavigationServiceEx_OnNavigated(object sender, NavigationEventArgs e)
+        {
+        }
+    }
+}
---
+++ src/MahAppsFrameNavigation/MahAppsFrameNavigation/Views/LoginPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using MahAppsFrameNavigation.Navigation;
+
+namespace MahAppsFrameNavigation.Views
+{
+    /// <summary>
+    /// Interaction logic for LoginPage.xaml
+    /// </summary>
+    public partial class LoginPage : Page
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginOnClick(object sender, RoutedEventArgs e)
+        {
+            var navigation = NavigationServiceEx.Instance;
+            navigation.Navigate(typeof(MainPage));
+            // or
+            //navigation.Navigate(new Uri("Views/MainPage.xaml", UriKind.RelativeOrAbsolute));
+        }
+    }
+}
